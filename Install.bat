c32edca1509625256ddd8ab9c4881fc087e1404f
Milquetoast <bebipbop.1@aol.com>
2018-03-27 20:52:27 -0400

    better install.bat feedback

--- Install.bat
+++ Install.bat
@@ -1,2 +1,14 @@
-start copy Half-Life.lnk "%userprofile%\desktop"
-start xcopy %cd% "C:\Program Files (x86)\Steam\steamapps\common\Half-Life" /E
+@echo off
+copy Half-Life.lnk "%userprofile%\desktop"
+cls
+echo Working...
+xcopy %cd% "C:\Program Files (x86)\Steam\steamapps\common\Half-Life" /E /Y /Q
+cls
+echo Crosshairs, player models, and the ProHL config file have been added to your Half-Life installation file. 
+echo You'll have new binds the next time you launch the game. Consider editing the [Change to Your Liking] tab 
+echo by opening prohl.cfg with notepad in your Half-Life folder. 
+echo.
+echo You'll then want to look over the [Binds] tab to get familliar with your new binds.
+echo.
+
+pause
